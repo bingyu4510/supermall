fb8f31bb0bf61cc776c37599cee945b8ff2c1479
bingyu4510 <61194925+bingyu4510@users.noreply.github.com>
2020-08-02 16:58:50 +0800

    Update request.js

--- src/network/request.js
+++ src/network/request.js
@@ -2,10 +2,8 @@ import axios from "axios"
 export function request(config) {
 
     const instance = axios.create({
-        // baseURL: 'http://123.207.32.32:8000',
-        // baseURL: 'http://123.207.32.32:8000/api/wh',
         // baseURL: 'http://106.54.54.237:8000/api/wh',
-        baseURL: 'http://123.207.32.32:8000/api/m3',
+        baseURL: 'http://127.127.32.32:8000/api/c3p0',
         timeout: 10000
     });
     instance.interceptors.response.use(rem => {
